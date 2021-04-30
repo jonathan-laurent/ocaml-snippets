67051f44c4efcee843c3bf90cdc9ee0c3710b286
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2021-04-30 12:30:23 -0400

    Add flambda benchs

--- flambda_benchs.ml
+++ flambda_benchs.ml
@@ -20,30 +20,59 @@ let rec add_assoc' ~compare ((k, v) as p) l =
 
 type t = A | B | C
 
-let f = function
+let enum_map = function
   | A -> A
   | B -> C
   | C -> B
 
-let f' = function
+let enum_map' = function
   | A as x -> x
   | B -> C
   | C -> B
 
-(* 30% slowdown *)
+(* Almost twice slower than the version that does not allocate *)
+let sum_squares l =
+  List.map l ~f:(fun x -> x * x)
+  |> List.fold ~init:0 ~f:(+)
+
+let sum_squares' l =
+  List.fold l ~init:0 ~f:(fun acc x -> acc + x * x)
+
+let compose_map l =
+  List.map l ~f:(fun x -> 2 * x) |> List.map ~f:(fun x -> x + 1)
+
+let compose_map' l = List.map l ~f:(fun x -> 2 * x + 1)
+
+let profile ~normal ~optimized =
+  let benchs = [
+    Bench.Test.create ~name:"Normal" normal;
+    Bench.Test.create ~name:"Optimized" optimized] in
+  benchs |> Bench.make_command |> Core.Command.run
+
+(* ~30% slowdown, with or without flambda *)
 let profile_add_assoc () =
   let l = List.init 10 ~f:(fun i -> (i, i)) in
   let p = (5, 5) in
-  let benchs = [
-    Bench.Test.create ~name:"Normal" (fun () -> add_assoc ~compare p l);
-    Bench.Test.create ~name:"Optimized" (fun () -> add_assoc' ~compare p l)] in
-  benchs |> Bench.make_command |> Core.Command.run
+  profile
+    ~normal:(fun () -> add_assoc ~compare p l)
+    ~optimized:(fun () -> add_assoc' ~compare p l)
 
 (* No difference *)
-let profile_f () =
-  let benchs = [
-    Bench.Test.create ~name:"Normal" (fun () -> f A);
-    Bench.Test.create ~name:"Optimized" (fun () -> f' A)] in
-  benchs |> Bench.make_command |> Core.Command.run
+let profile_enum_map () =
+  profile
+    ~normal:(fun () -> enum_map A)
+    ~optimized:(fun () -> enum_map' A)
 
-let () = profile_f ()
+let profile_sum_squares () =
+  let l = [1; 2; 3; 4; 5; 6] in
+  profile
+    ~normal:(fun () -> sum_squares l)
+    ~optimized:(fun () -> sum_squares' l)
+
+let profile_compose_map () =
+  let l = [1; 2; 3; 4; 5; 6] in
+  profile
+    ~normal:(fun () -> compose_map l)
+    ~optimized:(fun () -> compose_map' l)
+
+let () = profile_add_assoc ()
