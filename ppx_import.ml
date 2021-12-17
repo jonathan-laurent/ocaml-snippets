aad82cfa82455c2b87c5b7e58e23853aaecddd4a
Jonathan Laurent <jonathan.laurent@cs.cmu.edu>
2021-12-17 13:26:26 -0500

    Clean snippets

--- ppx_import.ml
+++
@@ -1,9 +0,0 @@
-type bigint = Z.t 
-let sexp_of_bigint i = String.sexp_of_t (Z.to_string i)
-let bigint_of_sexp s = Z.of_string (String.t_of_sexp s)
-
-type rational = Q.t = {num: bigint; den: bigint} [@@deriving sexp]
-
-let () =
-  Q.(of_int 1 / of_int 2)
-    |> [%sexp_of: rational] |> Sexp.to_string_hum |> Stdio.print_endline
